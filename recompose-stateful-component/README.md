1a844978d0ccbc268fd83259b57df7f3105c8c10
Max Hallinan <maxhallinan@riseup.net>
2017-08-03 16:21:57 +0200

    Add recompose-stateful-component

---
+++ recompose-stateful-component/README.md
@@ -0,0 +1,10 @@
+# recompose-stateful-component
+
+## Usage
+
+```
+# install create-react-app
+npm install -g create-react-app@1.3.3
+yarn install
+yarn start
+```
---
+++ recompose-stateful-component/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const StateLess = ({ title, }) => ();
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Page</h1>
+    );
+  }
+}
+
+export default App;
