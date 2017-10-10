3dc71701dde03e4163eda3ac51bfbd048231c9e5
Max Hallinan <maxhallinan@riseup.net>
2017-10-10 08:42:04 +0200

    Add more to elm workshop exercise

--- elm/front-end-masters/Main.elm
+++ elm/front-end-masters/Main.elm
@@ -4,14 +4,40 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+model =
+    { result =
+        { id = 1
+        , name = "TheSeamau5/elm-checkerboardgrid-tutorial"
+        , stars = 66
+        }
+    }
+
+
+update msg model =
+    if msg.operation === "SHOW_MORE" then
+        { maxResults = model.maxResults + msg.data }
+    else
+        model
+
+
 main =
-    div [ class "content" ]
-        [ header []
-            [ h1 []
-                [ text "ElmHub"
+    let
+        elmHubHeader =
+            header []
+                [ h1 [] [ text "ElmHub" ]
+                , span [ class "tagline" ] [ text "Like GitHub, but for Elm things." ]
                 ]
-            , span [ class "tagline" ]
-                [ text "Like GitHub, but for Elm things."
+    in
+        div [ class "content" ]
+            [ elmHubHeader
+            , ul [ class "results" ]
+                [ li []
+                    [ span [ class "star-count" ]
+                        [ text (toString model.result.stars) ]
+                    , a [ href ("https://github.com/" ++ model.result.name) ]
+                        [ text model.result.name ]
+                    ]
+
+                -- TODO create a link to the project
                 ]
             ]
-        ]
